a531ea67d18e8b72de399be20f64af1aff2931ec
Facundo Garay <5675299@mec.edu.py>
2024-01-25 09:00:54 -0300

    feat: Add game logic

--- scripts/gameLogic.js
+++ scripts/gameLogic.js
@@ -42,11 +42,10 @@ function createPlayer(marker) {
 const check = (function () {
   let marker;
   const IN_A_ROW = 3;
-  const boardGame = board.get();
   function row(y) {
     let matches = 0;
     for (let x = 0; x < IN_A_ROW; x++)
-      if (boardGame[y][x] === marker)
+      if (board.get()[y][x] === marker)
         matches++;
     if (matches === IN_A_ROW) return true;
     return false;
@@ -55,7 +54,7 @@ const check = (function () {
   function column(x) {
     let matches = 0;
     for (let y = 0; y < IN_A_ROW; y++)
-      if (boardGame[y][x] === marker)
+      if (board.get()[y][x] === marker)
         matches++;
     if (matches === IN_A_ROW) return true;
     return false;
@@ -65,23 +64,22 @@ const check = (function () {
     let matches = 0;
     let y = 0;
     for (let xy = 0; xy < IN_A_ROW; xy++)
-      if (boardGame[xy][xy] === marker)
+      if (board.get()[xy][xy] === marker)
         matches++;
     if (matches === IN_A_ROW) return true;
     matches = 0;
     for (let x = IN_A_ROW - 1; x >= 0; x--) {
-      if (boardGame[y][x] === marker)
+      if (board.get()[y][x] === marker)
         matches++;
       y++;
     }
     if (matches === IN_A_ROW) return true
     return false
   }
-  const setMarker = (mark) => marker = mark;
 
-  function all(x, y, marker) {
-    setMarker(marker);
-    if (row(y) || column(x) || cross()) return true
+  function all(x, y, mark) {
+    marker = mark;
+    if (row(y) || column(x) || cross()) return true;
     return false;
   }
 
@@ -89,11 +87,47 @@ const check = (function () {
 })();
 
 const game = (function () {
-  function shift(player) {
-
+  let winner = null;
+  let over = false;
+  function shift(player, player2) {
+    const X = 0;
+    const Y = 1;
+    const coordinates = player.getCoordinates();
+    board.put(coordinates[X], coordinates[Y], player.getMarker());
+    player.setShift(false);
+    player2.setShift(true);
+    if (check.all(coordinates[X], coordinates[Y], player.getMarker())) return true;
+    return false;
   }
   function round(player1, player2) {
-
+    if (player1.getShift()) {
+      if (shift(player1, player2)) {
+        winner = player1;
+        over = true;
+      }
+    }
+    else {
+      if (shift(player2, player1)) {
+        winner = player2;
+        over = true;
+      }
+    }
   }
-  function reset() { }
+
+  function play(player1, player2) {
+    let rounds = 0;
+    while (rounds < 9 && !over) {
+      round(player1, player2);
+      rounds++;
+    }
+    if (winner != null)
+      alert(`The winner is ${winner.getMarker()}`)
+    else
+      alert("It's a tie");
+  }
+
+  function reset() {
+    board.clear();
+  }
+  return { play, reset }
 })();
